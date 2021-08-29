461a90601cf8676ca56d6a257fde1f477582a1dd
Tsip2021 <tsuii@our.ecu.edu.au>
2021-08-29 14:38:20 +0800

    Task 1

---
+++ countdf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Student Name: Tsui Shan IP
+#Student ID: 10543651
+
+#Task 3:
+#Counts the number of files and child directories within a specific directory according to a specified property
+
+#Begin of the script
