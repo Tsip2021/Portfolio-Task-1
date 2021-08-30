2dd0d7683184fe1a9f925122de02f76e73a297b3
Tsip2021 <tsuii@our.ecu.edu.au>
2021-08-30 18:19:49 +0800

    Task 1

--- calcsum.sh
+++ calcsum.sh
@@ -4,19 +4,18 @@
 #Student ID: 10543651
 
 #Task 1: 
-#Calculates the sum of three (3) integers entered through the command line.
+#Calculates the sum of three (3) integers entered through the command line
 #If the sum exceeds 30, give the user a warning message and exit the script
 
-#Calculates the sum of three integers that user entered through the command line and assign it to a new variable: sum
+#Calculates the sum of three integers that user entered through the command line and save it to a new variable: sum
 sum=$(($1+$2+$3))
 
-#Check whether the sum is less than or equal to 30
-if [[ $sum -le 30 ]]; then
+if [[ $sum -le 30 ]]; then #Check whether the sum is less than or equal to 30
     echo "The sum of $1 and $2 and $3 is $sum" #Message if the sum is less than or equal to 30
-    exit 0
+    exit 0 #Exit the script without errors
 else
     echo "Sum exceeds maximum allowable" #Message if the sum is greater than 30
-    exit 1
+    exit 1 #Exit the script with errors
 fi
 
 exit 0
